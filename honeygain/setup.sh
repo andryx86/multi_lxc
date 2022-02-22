025732b637371f88b4e8b16203fd82c1bf547dbd
andryx86 <andryx86@gmail.com>
2022-02-22 09:29:56 +0100

    Update setup.sh

--- honeygain/setup.sh
+++ honeygain/setup.sh
@@ -53,7 +53,7 @@ sh <(curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o 
 
  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 
 # Customize Docker configuration
